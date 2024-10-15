f734619cdf92cc3840b9532096518c308849c8dc
Arne Van der Stede <arne.vanderstede@student.odisee.be>
2024-10-15 13:50:20 +0200

    Combined NFC and mqqt server

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include "WiFi.h"
 #include "PubSubClient.h" //pio lib install "knolleary/PubSubClient"
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_PN532.h>
 
 #define SSID          "IB3"
 #define PWD           "ingenieursbeleving3"
 
-#define MQTT_SERVER   "10.128.48.25"
+#define MQTT_SERVER   "192.168.0.110"
 #define MQTT_PORT     1883
 
 #define LED_PIN       2
@@ -15,6 +18,19 @@ long lastMsg = 0;
 char msg[50];
 int value = 0;
 
+#define PN532_IRQ   4
+#define PN532_RESET 5 
+
+const int DELAY_BETWEEN_CARDS = 500;
+long timeLastCardRead = 0;
+boolean readerDisabled = false;
+int irqCurr;
+int irqPrev;
+
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+
+static void startListeningToNFC();
+static void handleCardDetected();
 void callback(char *topic, byte *message, unsigned int length);
 
 void setup_wifi()
@@ -44,6 +60,23 @@ void setup()
   setup_wifi();
   client.setServer(MQTT_SERVER, MQTT_PORT);
   client.setCallback(callback);
+
+  nfc.begin();
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN532 board");
+    while (1); // halt
+  }
+
+    // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
+  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+  
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+
+  startListeningToNFC();
 
   pinMode(LED_PIN, OUTPUT);
 }
@@ -120,4 +153,70 @@ void loop()
   {
     lastMsg = now;
   }
+
+  if (readerDisabled) {
+    if (millis() - timeLastCardRead > DELAY_BETWEEN_CARDS) {
+      readerDisabled = false;
+      startListeningToNFC();
+    }
+  } else {
+    irqCurr = digitalRead(PN532_IRQ);
+
+    // When the IRQ is pulled low - the reader has got something for us.
+    if (irqCurr == LOW && irqPrev == HIGH) {
+       //Serial.println("Got NFC IRQ");  
+       handleCardDetected(); 
+    }
+  
+    irqPrev = irqCurr;
+  }
+}
+
+
+void startListeningToNFC() {
+  // Reset our IRQ indicators
+  irqPrev = irqCurr = HIGH;
+  
+  Serial.println("Present an ISO14443A Card ...");
+  nfc.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A);
+}
+
+void handleCardDetected() {
+    uint8_t success = false;
+    uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+    uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+    // read the NFC tag's info
+    success = nfc.readDetectedPassiveTargetID(uid, &uidLength);
+    Serial.println(success ? "Read successful" : "Read failed (not a card?)");
+
+    if (success) {
+      // Display some basic information about the card
+      //Serial.println("Found an ISO14443A card");
+      //Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+      //Serial.print("  UID Value: ");
+      Serial.print("Card ID HEX Value: ");
+      nfc.PrintHex(uid, uidLength);
+      
+      if (uidLength == 4)
+      {
+        // We probably have a Mifare Classic card ... 
+        uint32_t cardid = uid[0];
+        cardid <<= 8;
+        cardid |= uid[1];
+        cardid <<= 8;
+        cardid |= uid[2];  
+        cardid <<= 8;
+        cardid |= uid[3]; 
+        //Serial.print("Seems to be a Mifare Classic card #");
+        Serial.print("Card ID NUMERIC Value: ");
+        Serial.println(cardid);
+      }
+      Serial.println("");
+
+      timeLastCardRead = millis();
+    }
+
+    // The reader will be enabled again after DELAY_BETWEEN_CARDS ms will pass.
+    readerDisabled = true;
 }
